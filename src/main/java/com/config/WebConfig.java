ae38719fb30aa309f5325f682c92975f616d2da9
空空如也 <hei564927@gmail.com>
2024-09-04 22:36:46 +0800

    部署到云服务器

--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -12,10 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     // 跨域请求的配置
     public void addCorsMappings(CorsRegistry registry) {
         String ACCESS_PORT = "8080";
-        String ACCESS_IP = "124.70.48.240";
+        // 华为云ECS服务器
+//        String ACCESS_IP = "124.70.48.240";
+        // 华为云Flexus服务器
+        String ACCESS_IP = "120.46.139.40";
         registry.addMapping("/**")
-//                .allowedOrigins("http://" + ACCESS_IP) // 设置允许的前端URL
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://" + ACCESS_IP) // 设置允许的前端URL
+//                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
